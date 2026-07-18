5137457cd71af32cc170b236ef8739b206f8b6bd
T <74899888+tslashd@users.noreply.github.com>
2026-07-18 12:27:54 +0300

    Fix OpenAPI build: remove explicit Microsoft.OpenApi, add explicit usings to operation filter, remove duplicate AddSwaggerGen

--- Auth0/AuthorizeCheckOperationFilter.cs
+++ Auth0/AuthorizeCheckOperationFilter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace SurfTimer.Api.Auth0
